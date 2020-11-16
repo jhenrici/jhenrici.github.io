701dd4038745572c1ea1f251bade1787f5e90784
jhenrici <58534545+jhenrici@users.noreply.github.com>
2020-11-16 00:46:45 -0500

    push

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,20 @@
 // Get saved data from sessionStorage
-let title = ""; // Your code here
-let lat_max = "";
-let lat_min = "";
-let lon_max = "";
-let lon_min = "";
+let title = sessionStorage.getItem('title'); // Your code here
+let lat_max = sessionStorage.getItem('lat_max');
+let lat_min = sessionStorage.getItem('lat_min');
+let lon_max = sessionStorage.getItem('lon_max');
+let lon_min = sessionStorage.getItem('lon_min');
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+const route_id = document.getElementById('title');
+const latMax = document.getElementById('lat_max');
+const latMin = document.getElementById('lat_min');
+const lonMax = document.getElementById('lon_max');
+const lonMin = document.getElementById('lon_min');
+
+route_id.innerHTML = title;
+latMax.innerHTML =lat_max;
+latMin.innerHTML = lat_min;
+lonMax.innerHTML = lon_max;
+lonMin.innerHTML = lon_min;
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,17 +1,23 @@
 const main = document.getElementById("main");
 
 function getBusRoute() {
-  let busRoute = ""; // Your code here
+  let busRoute = document.getElementsByTagName('input')[0].value; // Your code here
 
   if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = ""; // Your code here
+    let busRouteURL = 'https://api.umd.io/v0/bus/routes/' + busRoute; // Your code here
+
 
     fetch(busRouteURL)
       .then((response) => {
         return response.json();
       })
       .then((route) => {
-        // YOUR CODE HERE
+        console.log(route);
+        sessionStorage.setItem('title', route.title);
+        sessionStorage.setItem('lat_max', route.lat_max);
+        sessionStorage.setItem('lat_min', route.lat_min);
+        sessionStorage.setItem('lon_max', route.lon_max);
+        sessionStorage.setItem('lon_min', route.lon_min);
       })
       .catch((err) => {
         console.log(err);
